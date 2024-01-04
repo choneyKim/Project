587f731a31b7ed2f112d272bd969a9507d6b880c
김치헌 <chk87a@gmail.com>
2024-01-04 21:14:03 +0900

    buyitem 매서드랑 player 클래스 연결

--- Program.cs
+++ Program.cs
@@ -21,8 +21,10 @@ public class Scene
 {
     private Player player;
     private Shop shop;
+    
     public Scene(Player player, Shop shop)
     {
+    
         this.player = player;
         this.shop = shop;
     }
@@ -45,7 +47,7 @@ public class Scene
             }
             else if (player.gold >= shop.items[selectNum - 1].Price)
             {
-                shop.playeritems.Add(shop.items[selectNum - 1]);
+                player.playeritems.Add(shop.items[selectNum - 1]);
                 player.gold -= shop.items[selectNum - 1].Price;
                 Console.Clear();
                 isselect = true;
@@ -313,10 +315,10 @@ public class Scene
     
 
 }
-
-
 public class Player
 {
+    public List<Item> playeritems = new List<Item>();
+
     public int level = 1;
     public string chad = "전사";
     public int attackPoint = 10;
@@ -324,30 +326,19 @@ public class Player
     public int health = 100;
     public int gold = 1500;
 
+    public int DisplayPlayerItems()
+    {
+        int itemnumber = 0;
+        foreach (var item in playeritems)
+        {
+            itemnumber++;
+            Console.WriteLine($"- {itemnumber} {item.Name}   | 방어력 +{item.Point}   | {item.Discription}     |  {item.Price} G");
+        }
+        return itemnumber;
+    }
+
 
 }
-//public class ItemList
-//{
-//    public List<Item> items = new List<Item>
-//        {
-//            new Item("무쇠갑옷", 9, "무쇠로 만들어져 튼튼한 갑옷입니다.", 1500),
-//            new Item("수련자 갑옷", 5, "수련에 도움을 주는 갑옷입니다.", 1000),
-//            new Item("스파르타의 갑옷", 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3000),
-//            new Item("낡은 검", 2, "쉽게 볼 수 있는 낡은 검입니다.", 600),
-//            new Item("청동 도끼", 5, "어디선가 사용했던 것 같은 도끼입니다.", 1500),
-//            new Item("스파르타의 창", 7, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 3000)
-//        };
-//    public int DisplayItems()
-//    {
-//        int itemnumber = 0;
-//        foreach (var item in items)
-//        {
-//            Console.WriteLine($"- {item.Name}   | 방어력 +{item.Point}   | {item.Discription}     |  {item.Price} G");
-//            itemnumber++;
-//        }
-//        return itemnumber;
-//    }
-//}
 public class Item
 {
     public string Name { get; set; }
@@ -366,7 +357,7 @@ public class Item
 }
 public class Shop
 {
-    public List<Item> playeritems = new List<Item>();
+
 
     public List<Item> items = new List<Item>
         {
